53bcdf38e885d246fb9c7451e5c9250086e3e84a
jessicalc <jessica@crayoncollective.com>
2015-07-21 02:12:03 +0800

    stringifyJSON WIP
    It ain't pretty.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -10,6 +10,21 @@ var isEmptyObj = function(obj) {
   return true;
 }; 
 
+var objToStr = function(obj) {
+  var str = "";
+  var objkey = "";
+  for (var key in obj) {
+    if (typeof obj[key] == "string") {
+      objkey = "\"" + obj[key] + "\"";
+    } else {
+      objkey = obj[key];
+    }
+    str = str + "\"" + [key] + "\"" + ":" + objkey + ",";
+  };
+  newStr = str.slice(0, -1);
+  return "{" + newStr + "}";
+};
+
 var stringifyJSON = function(obj) {
   if (typeof obj == "number") {
     return obj.toString();
@@ -19,6 +34,9 @@ var stringifyJSON = function(obj) {
   }
   if (obj == true) {
     return "true";
+  }
+  if (!Array.isArray(obj) && isEmptyObj(obj)) {
+    return "{}";
   }
   if (Array.isArray(obj) == false && obj == false) {
     return "false";
@@ -32,21 +50,30 @@ var stringifyJSON = function(obj) {
   if (Array.isArray(obj)) {
     var newArray = [];
     for (var i = 0; i < obj.length; i++) {
-      if (typeof obj[i] === "string") {
+      if (typeof obj[i] === "string" ) {
         newArray.push("\"" + obj[i] + "\"");
-      } else if (typeof obj[i] === "function") {
-        newArray.push("null");
-      } else {
+      } else if (typeof obj[i] === "number") {
         newArray.push(obj[i]);
+      } else if (Array.isArray(obj)) {
+        var newThing = obj[i].slice();
+        newArray.push(stringifyJSON(newThing));
       }
     }
-    return "[" + newArray + "]"; 
+    return "[" + newArray + "]";
   }
-//   else {
-//     var newObj = {};
-//     for (var key in obj) {
-//       newObj["\"" + key + "\""] = obj[key];
-//     }
-//     return newObj;
-//   }
+  if (typeof obj === "object") {
+    var newObj = {};
+    for (var key in obj) {
+      if (typeof obj[key] !== "object")
+        newObj[key] = obj[key];
+      if (obj[key] == null)
+        newObj[key] = null;
+      else if (typeof obj[key] === "object")
+        newObj[key] = stringifyJSON(obj[key]);
+    }
+  }
+  return objToStr(newObj);
+  return "[" + newArray + "]"; 
 };
+
+
